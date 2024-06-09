10a0f13eecbedc0c4cd14e099e4d552fe57898e1
josh handelman <handelmanjos@gmail.com>
2024-06-09 14:36:31 -0400

    stuff

--- programs/options/src/lib.rs
+++ programs/options/src/lib.rs
@@ -1,70 +1,204 @@
 
+
+
 use std::collections::HashMap;
 
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount};
-use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use anchor_spl::token::{Mint, Token, TokenAccount, transfer, Transfer, mint_to, MintTo};
 
-declare_id!("5eZCdZaW9kZFRZgC5kaHXQ1Tj4dAG6ythhcpS6hrp2fQ");
+declare_id!("BfrkttNPsNutRR3PKtsh8N2cN3EhkqXJWRwG5RSMU8AK");
+/* 
+    definitions:
+    principal token : token used to buy options
+    option token : token representing an option
+    underlying token : token that the option is for.
 
+    pairing of option token : principal token
+    option token is tradeable for underlying token
+*/
+const OPTION_MINT_DECIMALS: u8 = 6;
 #[program]
 pub mod options {
+
     use super::*;
-    
-    pub fn create(ctx: Context<Create>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
-    pub fn delete(ctx: Context<Delete>) -> Result<()> {
+    // should initialize an option token, set its data, and mint it to the user
+    // if option token already exists, should mint it to the user
+    // should take underlying token from the user and hold as collateral. 
+    pub fn create(ctx: Context<Create>, end_time: u64, strike_price: u64, amount: u64) -> Result<()> {
+        // transfer underlying from user to token account
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.user_underlying_token_account.to_account_info(),
+                    to: ctx.accounts.underlying_token_account.to_account_info(),
+                    authority: ctx.accounts.signer.to_account_info(),
+                }
+            ),
+            amount,
+        )?;
+        // mint option token to user
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.option_mint.to_account_info(),
+                    to: ctx.accounts.user_option_token_account.to_account_info(),
+                    authority: ctx.accounts.program_authority.to_account_info(),
+                },
+                &[&[b"auth", &[ctx.bumps.program_authority]]]
+            ),
+            amount,
+        )?;
+        ctx.accounts.option_data_account.end_time = end_time;
+        ctx.accounts.option_data_account.strike_price = strike_price;
+        ctx.accounts.option_data_account.amount_unexercised = amount;
+        ctx.accounts.option_data_account.creator = ctx.accounts.signer.key();
+        ctx.accounts.option_data_account.underlying_mint = ctx.accounts.underlying_mint.key();
         Ok(())
     }
-    pub fn mint(ctx: Context<Buy>) -> Result<()> {
+    pub fn list(ctx: Context<List>, amount: u64, price: u64) -> Result<()> {
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.user_option_token_account.to_account_info(),
+                    to: ctx.accounts.program_holder_account.to_account_info(),
+                    authority: ctx.accounts.signer.to_account_info(),
+                }
+            ),
+            amount,
+        )?;
+        ctx.accounts.list_account.amount += amount;
+        ctx.accounts.list_account.price = price;
+        ctx.accounts.list_account.owner = ctx.accounts.signer.key();
+        ctx.accounts.list_account.underlying_mint = ctx.accounts.option_data_account.underlying_mint.key();
+        ctx.accounts.list_account.option_mint = ctx.accounts.option_mint.key();
         Ok(())
     }
     pub fn exercise(ctx: Context<Exercise>) -> Result<()> {
         Ok(())
-    }   
-    pub fn crank(ctx: Context<Crank>) -> Result<()> {
-        Ok(())
     }
 }
-
+#[error_code]
+pub enum CustomError {
+    #[msg("Strike price not reached")]
+    StrikePriceNotReached,
+    #[msg("Token price not found")]
+    TokenPriceNotFound
+}
 #[account]
-pub struct OptionAccount {
-    pub options: HashMap<Pubkey, IndividualOption>,
-}
-#[derive(BorshSerialize, BorshDeserialize, BorshSchema, Clone)]
-pub struct IndividualOption {
-    pub creator: Pubkey,
-    pub amount: u64,
-    pub expiry: u64,
-    pub token_price: u64,
-    pub sol_price: u64,
+pub struct OptionDataAccount {
+    creator: Pubkey,
+    underlying_mint: Pubkey,
+    end_time: u64,
+    strike_price: u64,
+    amount_unexercised: u64,
 }
 
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        seeds = [b"auth"],
+        bump,
+        payer = signer,
+        space = 8
+    )]
+    /// CHECK: 
+    pub program_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
 #[derive(Accounts)]
 pub struct Create<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
+    pub underlying_mint: Account<'info, Mint>,
     #[account(mut)]
-    pub principal_token_account: Account<'info, TokenAccount>,
-    pub token_mint: Account<'info, Mint>,
+    pub user_underlying_token_account: Account<'info, TokenAccount>,
     #[account(
         init_if_needed,
         payer = signer,
-        seeds = [b"holder", token_mint.key().as_ref()],
+        seeds = [b"underlying_token", underlying_mint.key().as_ref()],
         bump,
-        token::mint = token_mint,
-        token::authority = program_authority
+        token::authority = program_authority,
+        token::mint = underlying_mint
     )]
-    pub holding_token_account: Account<'info, TokenAccount>,
+    pub underlying_token_account: Account<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = signer,
+        mint::authority = program_authority,
+        mint::decimals = OPTION_MINT_DECIMALS,
+    )]
+    pub option_mint: Account<'info, Mint>,
+    #[account(
+        init,
+        payer = signer,
+        token::authority = signer,
+        token::mint = option_mint,
+    )]
+    pub user_option_token_account: Account<'info, TokenAccount>,
+    #[account(
+        init,
+        seeds = [b"option_data_account", option_mint.key().as_ref()],
+        bump,
+        payer = signer,
+        space = 8 + 32 + 32 + 8 + 8 + 8,
+    )]
+    pub option_data_account: Account<'info, OptionDataAccount>,
+    #[account(
+        seeds = [b"auth"],
+        bump
+    )]
+    /// CHECK: 
+    pub program_authority: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+#[account]
+pub struct Listing {
+    underlying_mint: Pubkey,
+    option_mint: Pubkey,
+    owner: Pubkey,
+    amount: u64,
+    price: u64,
+}
+#[derive(Accounts)]
+#[instruction(price: u64)]
+pub struct List<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub option_mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub user_option_token_account: Account<'info, TokenAccount>,
+    #[account(
+        seeds = [b"option_data_account", option_mint.key().as_ref()],
+        bump
+    )]
+    pub option_data_account: Account<'info, OptionDataAccount>,
     #[account(
         init_if_needed,
         payer = signer,
-        seeds = [b"option", token_mint.key().as_ref()],
+        seeds = [b"holder_account", option_mint.key().as_ref()],
         bump,
-        space = 8,
+        token::authority = program_authority,
+        token::mint = option_mint
     )]
-    pub option: Box<Account<'info, OptionAccount>>,
+    pub program_holder_account: Account<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        seeds = [b"listing", option_mint.key().as_ref(), signer.key().as_ref(), price.to_le_bytes().as_ref()],
+        bump,
+        space = 8 + 32 + 32 + 32 + 8 + 8
+    )]
+    pub list_account: Account<'info, Listing>,
     #[account(
         seeds = [b"auth"],
         bump,
@@ -75,19 +209,8 @@ pub struct Create<'info> {
     pub token_program: Program<'info, Token>,
 }
 #[derive(Accounts)]
-pub struct Delete {
-
+pub struct Exercise<'info> {
+    pub signer: Signer<'info>,
 }
-#[derive(Accounts)]
-pub struct Buy {
 
-}
-#[derive(Accounts)]
-pub struct Exercise {
-
-}
-#[derive(Accounts)]
-pub struct Crank {
-
-}
 
--- tests/options.ts
+++ tests/options.ts
@@ -1,16 +1,72 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Options } from "../target/types/options";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import {createMint, getAssociatedTokenAddressSync, getOrCreateAssociatedTokenAccount, mintTo} from "@solana/spl-token";
 
 describe("options", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet
   const program = anchor.workspace.Options as Program<Options>;
-
-  it("Is initialized!", async () => {
+  const [programAuthority] = PublicKey.findProgramAddressSync(
+    [Buffer.from("auth")],
+    program.programId,
+  )
+  const OPTION_DECIMALS: number = 6;
+  const MINT_AMOUNT: number = 100000 * 10 ** OPTION_DECIMALS;
+  const mintToken = async () => {
+    const mint = await createMint(
+      provider.connection,
+      wallet.payer,
+      wallet.publicKey,
+      null,
+      OPTION_DECIMALS,
+    )
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey,
+    );
+    await mintTo(
+      provider.connection,
+      wallet.payer,
+      mint,
+      tokenAccount.address,
+      wallet.payer,
+      MINT_AMOUNT
+    );
+    return { tokenAccount: tokenAccount.address, mint }
+  };
+  it("initialized", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    await program.methods.initialize().rpc();
+  });
+  it("creates option mint", async () => {
+    const { mint: underlyingMint, tokenAccount: userUnderlyingTokenAccount } = await mintToken();
+    const [underlyingTokenAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("underlying_token"), underlyingMint.toBuffer()],
+      program.programId
+    );
+    const optionMint = Keypair.generate();
+    const userOptionTokenAccount = getAssociatedTokenAddressSync(optionMint.publicKey, wallet.publicKey);
+    const [optionDataAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("option_data_account"), optionMint.publicKey.toBuffer()],
+      program.programId,
+    );
+    const date = Date.now() + 1000000;
+    await program.methods.create(new anchor.BN(date), 200, 400 * 10 ** OPTION_DECIMALS).accounts({
+      signer: wallet.publicKey,
+      underlyingMint,
+      userUnderlyingTokenAccount,
+      underlyingTokenAccount,
+      optionMint: optionMint.publicKey,
+      userOptionTokenAccount,
+      optionDataAccount,
+      programAuthority,
+    }).rpc();
+    const optionData = await program.account.optionDataAccount.fetch(optionDataAccount);
   });
 });
